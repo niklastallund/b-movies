5948d58f8239b1a61ba1c642e0bbe963fbd019bf
Niklas Tallund <niklastallund@gmail.com>
2025-08-27 14:29:59 +0200

    sign-up working

--- src/app/sign-up/page.tsx
+++ src/app/sign-up/page.tsx
@@ -1,4 +1,9 @@
+import SignUpForm from "@/components/sign-up-form";
 
 export default function SignUpPage() {
-  return <div>Sign up page!</div>
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <SignUpForm />
+    </div>
+  );
 }
---
+++ src/components/sign-up-form.tsx
@@ -0,0 +1,151 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { authClient } from "@/lib/auth-client";
+
+const FormSchema = z
+  .object({
+    name: z.string().min(2).max(50),
+    email: z.email().max(100),
+    password: z.string().min(6).max(100),
+    confirmPass: z.string(),
+  })
+  .refine((values) => values.password === values.confirmPass, {
+    message: "Passwords do not match",
+    path: ["confirmPass"],
+  });
+
+type FormValues = z.infer<typeof FormSchema>;
+
+export default function SignUpForm() {
+  const form = useForm<FormValues>({
+    resolver: zodResolver(FormSchema),
+    defaultValues: {
+      name: "",
+      email: "",
+      password: "",
+      confirmPass: "",
+    },
+  });
+
+  async function onSubmit(values: FormValues) {
+    const { error } = await authClient.signUp.email({
+      name: values.name,
+      email: values.email,
+      password: values.password,
+    });
+
+    if (error) {
+      alert(error.message);
+    } else {
+      //TODO reroute to user profile (or somewhere else?)
+      alert("Success!");
+    }
+  }
+
+  return (
+    <Card className="w-full max-w-sm">
+      <CardHeader>
+        <CardTitle>Sign Up</CardTitle>
+        <CardDescription>
+          Enter your information below to create an account
+        </CardDescription>
+        <CardAction>
+          <Button variant="link">
+            Sign In
+            {/* TODO this button needs to link to the sign in component */}
+          </Button>
+        </CardAction>
+      </CardHeader>
+      <CardContent>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-3">
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Username</FormLabel>
+                  <FormControl>
+                    <Input {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Email</FormLabel>
+                  <FormControl>
+                    <Input type="email" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={form.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Password</FormLabel>
+                  <FormControl>
+                    <Input type="password" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={form.control}
+              name="confirmPass"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Confirm Password</FormLabel>
+                  <FormControl>
+                    <Input type="password" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <Button
+              type="submit"
+              className="w-auto"
+              disabled={form.formState.isSubmitting}
+            >
+              Create Account
+            </Button>
+          </form>
+        </Form>
+      </CardContent>
+    </Card>
+  );
+}
