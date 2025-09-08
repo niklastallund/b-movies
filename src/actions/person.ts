d7b26f08eacdda431c5cd168ba5a51f53b687c84
Niklas Tallund <niklastallund@gmail.com>
2025-09-08 15:51:17 +0200

    added some person query functions

--- src/actions/person.ts
+++ src/actions/person.ts
@@ -7,11 +7,10 @@ import {
   updatePersonSchema,
 } from "@/lib/zod-schemas";
 
+// !!!!!!!!!!!!!!!!!!
+// !TODO auth checks!
+// !!!!!!!!!!!!!!!!!!
 export async function createPerson(person: CreatePersonInput) {
-  // !!!!!!!!!!!!!!!!!!
-  // !TODO auth checks!
-  // !!!!!!!!!!!!!!!!!!
-
   const data = await createPersonSchema.parseAsync(person);
 
   const newPerson = await prisma.person.create({
@@ -29,10 +28,6 @@ export async function createPerson(person: CreatePersonInput) {
 }
 
 export async function updatePerson(person: UpdatePersonInput) {
-  // !!!!!!!!!!!!!!!!!!
-  // !TODO auth checks!
-  // !!!!!!!!!!!!!!!!!!
-
   const data = await updatePersonSchema.parseAsync(person);
 
   const updatedPerson = await prisma.person.update({
@@ -48,4 +43,51 @@ export async function updatePerson(person: UpdatePersonInput) {
   });
 
   return updatedPerson;
+}
+
+export async function deletePerson(id: number) {
+  const deletedPerson = await prisma.person.delete({
+    where: { id },
+  });
+
+  return deletedPerson;
+}
+
+export async function getAllPersons() {
+  const persons = await prisma.person.findMany();
+  return persons;
+}
+
+export async function getPersonById(id: number) {
+  const person = await prisma.person.findUnique({
+    where: { id },
+  });
+  return person;
+}
+
+export async function getPersonByTmdbId(tmdbId: number) {
+  const person = await prisma.person.findUnique({
+    where: { tmdbId },
+  });
+  return person;
+}
+
+export async function getPersonsByName(name: string) {
+  const persons = await prisma.person.findMany({
+    where: {
+      name: {
+        contains: name,
+        mode: "insensitive",
+      },
+    },
+  });
+  return persons;
+}
+
+export default function getPersonsByBirthday(birthday: Date) {
+  return prisma.person.findMany({
+    where: {
+      birthday: birthday,
+    },
+  });
 }
